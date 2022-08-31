633c8aaa199b75383dce1f237d232d1565656a3d
Bin <bin@cacdi.com>
2022-08-31 17:17:01 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "mind-ar/dist/mindar-image.prod.js";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
+import "./App.css";
+import MindARViewer from "./mindar-viewer";
 
 function App() {
+  const [started, setStarted] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MindARViewer />
     </div>
   );
 }
